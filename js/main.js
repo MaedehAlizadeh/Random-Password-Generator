ef4e294247e8e33a612be2b03cc3b19636171922
m.alizadeh <alizadee.maedeh@gmail.com>
2022-05-13 16:38:09 +0430

    Completed Password Generator

--- js/main.js
+++ js/main.js
@@ -1,4 +1,25 @@
 //Template Name : Random Password Generator
 //Author Name : Maedeh Alizadeh
 
+const randomPass = document.querySelector('.form-control');
+const passGenBtn = document.querySelector('.generate-btn');
+const copyBtn = document.querySelector('.copy-btn')
+
+passGenBtn.addEventListener('click' , generatePass);
+
+
+function generatePass(event) {
+    event.preventDefault();
+    const char = '1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let pass = '';
+    const passLength = 16;
+
+    for( let i = 0 ; i <= passLength ; i++) {
+        let randomChar = char.charAt(Math.floor(Math.random() * char.length));
+        pass += randomChar;
+    }
+
+    randomPass.value = pass;
+    
+}
 
